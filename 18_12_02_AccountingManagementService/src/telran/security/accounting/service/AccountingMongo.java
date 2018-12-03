03bf18b57876f3028747ba1455b529d3f536222c
Beliy <i.belostotskiy@gmail.com>
2018-12-03 15:26:34 +0200

    Commit 03/12/18

--- 18_12_02_AccountingManagementService/src/telran/security/accounting/service/AccountingMongo.java
+++ 18_12_02_AccountingManagementService/src/telran/security/accounting/service/AccountingMongo.java
@@ -1,8 +1,13 @@
 package telran.security.accounting.service;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Set;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import telran.security.accounting.Account;
@@ -14,10 +19,20 @@ public class AccountingMongo implements IAccounting{
 @Autowired
 IAccountingMongoRepository accountsRepo;
 
+@Value("${expiration_period:1}")
+int expirationPeriod;
+
+@PostConstruct
+public void displayValue() {
+	System.out.println(expirationPeriod);
+}
+
 @Override
 public String getPassword(String username) {
 	Account account=accountsRepo.findById(username).orElse(null);
-	return account==null?null:account.getPassword_hash();
+	if((ChronoUnit.DAYS.between(account.getActivationDate(), LocalDate.now()) < expirationPeriod) && !account.isRevoked() && account != null)
+		return account.getPassword_hash();
+	else return null;
 }
 
 @Override
